bfd7eb3fa1d40d2a401aae41954dc0893cb111e4
Brian Armstrong <barmstrong@gmail.com>
2015-12-04 12:32:02 -0800

    debugging app.json

--- initializers/redis.rb
+++ initializers/redis.rb
@@ -1,3 +1,2 @@
 require 'redis'
-raise "redis needs to be provisioned" if ENV["REDIS_URL"].nil?
-$redis = Redis.new(url: ENV["REDIS_URL"])
+$redis = Redis.new(url: ENV["REDIS_URL"] || ENV["REDISTOGO_URL"])
